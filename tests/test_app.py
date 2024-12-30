43d94e98e25f82f4d817c2e7a529c58a10f2a577
Scriptaron <aaron.manzone@gmail.com>
2024-12-29 23:47:03 -0300

    Configuração inicial do projeto

---
+++ tests/test_app.py
@@ -0,0 +1,14 @@
+from http import HTTPStatus
+
+from fastapi.testclient import TestClient
+
+from fast_zero.app import app
+
+
+def test_root_deve_retornar_ok_e_ola_mundo():
+    client = TestClient(app)  # Arrange (Organizar)
+
+    response = client.get('/')  # Act (Agir)
+
+    assert response.status_code == HTTPStatus.OK  # Assert (Afirmar)
+    assert response.json() == {'message': 'Olá Mundo!'}  # Assert (Afirmar)
